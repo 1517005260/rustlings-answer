8da6270908674087588d0b1248f007e1ecfd53ee
GLK <1517005260@qq.com>
2024-10-22 12:47:33 +0800

    done quiz 2

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -5,29 +5,35 @@
 // - Modules
 // - Enums
 //
-// Let's build a little machine in the form of a function. As input, we're going
-// to give a list of strings and commands. These commands determine what action
-// is going to be applied to the string. It can either be:
-// - Uppercase the string
-// - Trim the string
-// - Append "bar" to the string a specified amount of times
-//
-// The exact form of this will be:
-// - The input is going to be a Vector of 2-length tuples,
-//   the first element is the string, the second one is the command.
-// - The output element is going to be a vector of strings.
+// 让我们以函数的形式构建一个小机器。作为输入，我们将提供一个字符串列表和命令。 这些命令决定了要对字符串执行的操作。它可以是：
+// - 将字符串转换为大写
+// - 去除字符串的首尾空格
+// - 向字符串末尾追加指定次数的“bar”
+// 它的具体形式将是：
+// - 输入将是一个由2元组组成的向量，第一个元素是字符串，第二个是命令。
+// - 输出将是一个字符串向量。
 
-enum Command {
+
+enum Command {  // 匹配不同操作态
     Uppercase,
     Trim,
     Append(usize),
 }
 
 mod my_module {
+    use std::io::repeat;
     use super::Command;
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input:Vec<(String, Command)>) -> Vec<String>{
+        input.into_iter()  // 先将 input 向量转换为迭代器，每次向后取一个
+            .map(|(s, Command)| match Command {  // map = "对于" ， 即对于每个 (s,cmd) 元组
+                Command::Uppercase => s.to_uppercase(),
+                Command::Trim => s.trim().to_string(),
+                Command::Append(n) => format!("{}{}", s, "bar".repeat(n)),
+            }).collect()  // collect 方法 将收集 map 产生的元素
+    }
 }
 
 fn main() {
@@ -39,6 +45,7 @@ mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
     use super::Command;
+    use super::my_module::*;  // super 约等于 linux的 ..
 
     #[test]
     fn it_works() {
