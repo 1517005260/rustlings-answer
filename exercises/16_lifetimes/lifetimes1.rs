580e754ee6ac925ec2d3cd455e762d6aa48904c1
GLK <1517005260@qq.com>
2024-10-23 10:23:47 +0800

    done 16_lifetimes

--- exercises/16_lifetimes/lifetimes1.rs
+++ exercises/16_lifetimes/lifetimes1.rs
@@ -4,12 +4,15 @@
 // not own their own data. What if their owner goes out of scope?
 
 // TODO: Fix the compiler error by updating the function signature.
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
         y
-    }
+    }  // 修复之前，没有生命周期。当函数尝试返回两个之一的引用的时候会报错。
+       // 因为编译器无法确定返回的引用的生命周期是否和 x 或 y 的生命周期一致，也无法判断它们是否可能在函数返回后已经无效
+       // x,y 都是引用来的，在本函数的作用域内不清楚它们的来历，所以需要显式声明来历
+       // 显然，单个引用时不需要生命周期
 }
 
 fn main() {
--- exercises/16_lifetimes/lifetimes2.rs
+++ exercises/16_lifetimes/lifetimes2.rs
@@ -1,5 +1,5 @@
 // Don't change this function.
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {  //fn longest<'a> 声明此处有生命周期
     if x.len() > y.len() {
         x
     } else {
@@ -11,10 +11,13 @@ fn main() {
     // TODO: Fix the compiler error by moving one line.
 
     let string1 = String::from("long string is long");
-    let result;
-    {
-        let string2 = String::from("xyz");
-        result = longest(&string1, &string2);
-    }
+    // let result;
+    // {
+    //     let string2 = String::from("xyz");
+    //     result = longest(&string1, &string2);    // 悬空引用：str2的作用域仅限于{}，而str1存在于整个main；result = &str1 or &str2
+    // }
+    // 所以作出修改：调整作用域即可
+    let string2 = String::from("xyz");
+    let result = longest(&string1, &string2);
     println!("The longest string is '{result}'");
 }
