06a5db005d6e1060934dc1d3a98e50b340edd559
GLK <1517005260@qq.com>
2024-10-27 18:54:08 +0800

    done 21_macros

--- exercises/21_macros/macros3.rs
+++ exercises/21_macros/macros3.rs
@@ -1,5 +1,6 @@
 // TODO: Fix the compiler error without taking the macro definition out of this
 // module.
+#[macro_use]  // 宏的作用域默认在模块内，需要加注解以在模块外部使用
 mod macros {
     macro_rules! my_macro {
         () => {
--- exercises/21_macros/macros4.rs
+++ exercises/21_macros/macros4.rs
@@ -1,9 +1,9 @@
 // TODO: Fix the compiler error by adding one or two characters.
-#[rustfmt::skip]
+#[rustfmt::skip]  // 用于告诉 Rustfmt（Rust的自动格式化工具）跳过对特定代码块的格式化
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };  // 连续定义宏需要分号
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
     }
